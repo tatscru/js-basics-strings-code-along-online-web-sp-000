5d064583c080f15c646385b8e44d893621184864
tatiana.mojica <tatiana.mojica@bustle.com>
2019-08-13 01:40:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = "Tats";
+// const welcomeMessage = "Welcome to Flatbook, "
+const welcomeMessage = `Welcome to Flatbook, + ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// const shortGreeting = `Welcome, ${currentUser[0]}`;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
