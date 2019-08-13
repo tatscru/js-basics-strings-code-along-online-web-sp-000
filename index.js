9cddedf60f69c33902a00449c768c7ea6cb673c4
tatiana.mojica <tatiana.mojica@bustle.com>
2019-08-13 01:32:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const currentUser = "Tats";
 const welcomeMessage = `Welcome to Flatbook, + ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = "unsure"
