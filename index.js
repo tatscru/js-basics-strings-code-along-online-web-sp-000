cfa562a6630e8418d3e3ab05b04c9a4d9d35db1d
tatiana.mojica <tatiana.mojica@bustle.com>
2019-08-13 01:23:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = "Tats";
 // const welcomeMessage = "Welcome to Flatbook, "
-const welcomeMessage = "Welcome to Flatbook, " + currentUser
+const welcomeMessage = "Welcome to Flatbook, " + currentUser;
